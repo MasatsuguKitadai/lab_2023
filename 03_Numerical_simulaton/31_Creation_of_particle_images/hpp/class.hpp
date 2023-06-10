9b562e5daceef8613cc3244ef1dac429f3ce884a
Masatsugu KITADAI <b8123026@gmail.com>
2023-06-10 13:13:45 +0900

    programs

--- 03_Numerical_simulaton/31_Creation_of_particle_images/hpp/class.hpp
+++ 03_Numerical_simulaton/31_Creation_of_particle_images/hpp/class.hpp
@@ -22,8 +22,8 @@ const string main_path = "/mnt/e/workspace_SSD/03_numerical_simulation/"; // SSD
 // const string main_path = "/mnt/d/workspace_HDD/03_numerical_simulation/"; // HDD 家のPC
 
 /* 主要パラメータ */
-const float deg = 16;          // 壁面の回転速度 [deg/s]
-const int num_per_image = 100; // 1枚あたりに映り込む粒子数 [個]
+const float deg = 10;          // 壁面の回転速度 [deg/s]
+const int num_per_image = 250; // 1枚あたりに映り込む粒子数 [個]
 
 /* 流れの条件 */
 const float flow_speed = 8.5;       // 流速 [mm/s]
