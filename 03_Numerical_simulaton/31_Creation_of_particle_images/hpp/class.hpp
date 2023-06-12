e570c7223ed92556684b27b4bedb6e78f45869ef
Masatsugu KITADAI <b8123026@gmail.com>
2023-06-12 12:48:25 +0900

    programs

--- 03_Numerical_simulaton/31_Creation_of_particle_images/hpp/class.hpp
+++ 03_Numerical_simulaton/31_Creation_of_particle_images/hpp/class.hpp
@@ -23,8 +23,8 @@ const string main_path = "/mnt/e/workspace_SSD/03_numerical_simulation/"; // SSD
 // const string main_path = "/mnt/d/workspace_HDD/03_numerical_simulation/"; // HDD 家のPC
 
 /* 主要パラメータ */
-const float deg = 10;          // 壁面の回転速度 [deg/s]
-const int num_per_image = 500; // 1枚あたりに映り込む粒子数 [個]
+const float deg = 12;         // 壁面の回転速度 [deg/s]
+const int num_per_image = 50; // 1枚あたりに映り込む粒子数 [個]
 
 /* 流れの条件 */
 const float flow_speed = 8.5;       // 流速 [mm/s]
@@ -68,7 +68,7 @@ const float lls_2_thickness = lls_1_thickness * 3.0;        // 後方のllsの�
 
 /* 粒子の生成範囲の設定 */
 const float range_x_max = lls_2_position + 0.05; // x方向の粒子生成範囲　[mm]
-const float range_x_min = lls_2_position - 1.5;  // x方向の粒子生成範囲　[mm]
+const float range_x_min = lls_2_position - 1.0;  // x方向の粒子生成範囲　[mm]
 // const float range_x_min = -250 * 0.003; // x方向の粒子生成範囲　[mm]
 
 const float range_x = range_x_max - range_x_min; // x方向の粒子生成範囲　[mm]
