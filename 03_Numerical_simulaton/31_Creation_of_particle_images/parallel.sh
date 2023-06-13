f3cee241af6fdc43d3109924fb117bfc92cd3d15
Masatsugu KITADAI <b8123026@gmail.com>
2023-06-13 14:53:15 +0900

    programs

---
+++ 03_Numerical_simulaton/31_Creation_of_particle_images/parallel.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+echo "Start\t:" `date '+%y/%m/%d %H:%M:%S'`
+TIME_A=`date +%s`   
+
+## プロセス(2)
+./out/numerical_simulation_300.out
+pid1=${!}
+sleep 1
+wait ${pid1}
+
+# 処理時間の計算
+TIME_B=`date +%s`
+
+PT=`expr ${TIME_B} - ${TIME_A}`
+H=`expr ${PT} / 3600`
+PT=`expr ${PT} % 3600`
+M=`expr ${PT} / 60`
+S=`expr ${PT} % 60`
+
+# Finish
+echo "Finish\t:" `date '+%y/%m/%d %H:%M:%S'`
+echo "Time\t: ${H}:${M}:${S}"
+
+#############################################################
+
+./out/numerical_simulation_350.out
+pid1=${!}
+sleep 1
+wait ${pid1}
+
+# 処理時間の計算
+TIME_B=`date +%s`
+
+PT=`expr ${TIME_B} - ${TIME_A}`
+H=`expr ${PT} / 3600`
+PT=`expr ${PT} % 3600`
+M=`expr ${PT} / 60`
+S=`expr ${PT} % 60`
+
+# Finish
+echo "Finish\t:" `date '+%y/%m/%d %H:%M:%S'`
+echo "Time\t: ${H}:${M}:${S}"
+
+#############################################################
+
+./out/numerical_simulation_400.out
+pid1=${!}
+sleep 1
+wait ${pid1}
+
+# 処理時間の計算
+TIME_B=`date +%s`
+
+PT=`expr ${TIME_B} - ${TIME_A}`
+H=`expr ${PT} / 3600`
+PT=`expr ${PT} % 3600`
+M=`expr ${PT} / 60`
+S=`expr ${PT} % 60`
+
+# Finish
+echo "Finish\t:" `date '+%y/%m/%d %H:%M:%S'`
+echo "Time\t: ${H}:${M}:${S}"
+
+#############################################################
+
+./out/numerical_simulation_450.out
+pid1=${!}
+sleep 1
+wait ${pid1}
+
+# 処理時間の計算
+TIME_B=`date +%s`
+
+PT=`expr ${TIME_B} - ${TIME_A}`
+H=`expr ${PT} / 3600`
+PT=`expr ${PT} % 3600`
+M=`expr ${PT} / 60`
+S=`expr ${PT} % 60`
+
+# Finish
+echo "Finish\t:" `date '+%y/%m/%d %H:%M:%S'`
+echo "Time\t: ${H}:${M}:${S}"
