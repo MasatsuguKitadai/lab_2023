ea2565ed029e0560dea2c2bc2a62f8a647d8ebdc
Masatsugu KITADAI <b8123026@gmail.com>
2023-06-22 08:18:43 +0900

    programs

--- 03_Numerical_simulaton/31_Creation_of_particle_images/ISTP_1-1.sh
+++ 03_Numerical_simulaton/31_Creation_of_particle_images/ISTP_1-1.sh
@@ -2,32 +2,32 @@ echo "Start\t:" `date '+%y/%m/%d %H:%M:%S'`
 TIME_A=`date +%s`   
 
 ## シミュレーション(1) ###
-for name in 13 14 15
+for num in 50 150 250 350
 do
+    # 粒子数の生成
     expect -c " 
     set timeout -1
-
     spawn out/numerical_simulation.out
-    expect \"Case Name:\"
-    send \"$name-300\n\"
+    expect \"Rotational speed :\"
+    send \"10\n\"
+    expect \"Particle number :\"
+    send \"$num\n\"
     expect \"$\n\"
-
     exit 0
     "
+
+    # 処理時間の計算
+    TIME_B=`date +%s`
+    PT=`expr ${TIME_B} - ${TIME_A}`
+    H=`expr ${PT} / 3600`
+    PT=`expr ${PT} % 3600`
+    M=`expr ${PT} / 60`
+    S=`expr ${PT} % 60`
+
+    # Finish
+    echo "Finish\t:" `date '+%y/%m/%d %H:%M:%S'`
+    echo "Time\t: ${H}:${M}:${S}"
 done
-
-# 処理時間の計算
-TIME_B=`date +%s`
-
-PT=`expr ${TIME_B} - ${TIME_A}`
-H=`expr ${PT} / 3600`
-PT=`expr ${PT} % 3600`
-M=`expr ${PT} / 60`
-S=`expr ${PT} % 60`
-
-# Finish
-echo "Finish\t:" `date '+%y/%m/%d %H:%M:%S'`
-echo "Time\t: ${H}:${M}:${S}"
 
 # 空フォルダの削除
 find /mnt/e/workspace_SSD/03_numerical_simulation/ -type d -empty | xargs rm -r20
---
+++ 03_Numerical_simulaton/31_Creation_of_particle_images/ISTP_1-3.sh
@@ -0,0 +1,35 @@
+echo "Start\t:" `date '+%y/%m/%d %H:%M:%S'`
+TIME_A=`date +%s`   
+
+speed = 10;
+
+## シミュレーション(1) ###
+for num in 450 500 
+do
+    # 粒子数の生成
+    expect -c " 
+    set timeout -1
+    spawn out/numerical_simulation.out
+    expect \"Rotational speed :\"
+    send \"10\n\"
+    expect \"Particle number :\"
+    send \"$num\n\"
+    expect \"$\n\"
+    exit 0
+    "
+
+    # 処理時間の計算
+    TIME_B=`date +%s`
+    PT=`expr ${TIME_B} - ${TIME_A}`
+    H=`expr ${PT} / 3600`
+    PT=`expr ${PT} % 3600`
+    M=`expr ${PT} / 60`
+    S=`expr ${PT} % 60`
+
+    # Finish
+    echo "Finish\t:" `date '+%y/%m/%d %H:%M:%S'`
+    echo "Time\t: ${H}:${M}:${S}"
+done
+
+# 空フォルダの削除
+find /mnt/e/workspace_SSD/03_numerical_simulation/ -type d -empty | xargs rm -r20
