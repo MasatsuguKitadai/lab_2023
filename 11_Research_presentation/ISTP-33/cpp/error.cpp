18e2daecd7b2db44beaa581c30c6c2cd08691fba
Masatsugu KITADAI <b8123026@gmail.com>
2023-06-27 18:19:35 +0900

    programs

--- 11_Research_presentation/ISTP-33/cpp/error.cpp
+++ 11_Research_presentation/ISTP-33/cpp/error.cpp
@@ -16,6 +16,8 @@ using namespace std;
 /** プロトタイプ宣言 **/
 void gnuplot_1();
 void gnuplot_2();
+void gnuplot_3();
+void gnuplot_4();
 
 /*****************************************************************************/
 
@@ -28,6 +30,8 @@ int main()
 {
     gnuplot_1();
     gnuplot_2();
+    gnuplot_3();
+    gnuplot_4();
 
     return 0;
 }
@@ -56,7 +60,7 @@ void gnuplot_1()
 
     // label
     const char *xxlabel = "Number of blue particles in one image : {/:Italic n_p} [-/frame]";
-    const char *yylabel = "RMSE ratio [%]";
+    const char *yylabel = "RMSE rate [%]";
 
     // Gnuplot 呼び出し
     if ((gp = popen("gnuplot", "w")) == NULL)
@@ -128,7 +132,7 @@ void gnuplot_2()
 
     // label
     const char *xxlabel = "Rotational speed : {/Symbol w} × 180 / {/Symbol p} [deg/s]";
-    const char *yylabel = "RMSE ratio [%]";
+    const char *yylabel = "RMSE rate [%]";
 
     // Gnuplot 呼び出し
     if ((gp = popen("gnuplot", "w")) == NULL)
@@ -168,6 +172,142 @@ void gnuplot_2()
     // グラフの出力
     fprintf(gp, "plot '%s' using 1:4 with points pt 7 ps 0.5 lc 'red' title '{/:Italic n_p} =   50', '%s' using 1:4 with lines lw 0.5 lc 'red' notitle, ", readfile_1, readfile_1);
     fprintf(gp, "'%s' using 1:4 with points pt 7 ps 0.5 lc 'blue' title '{/:Italic n_p} = 300', '%s' using 1:4 with lines lw 0.5 lc 'blue' notitle\n", readfile_2, readfile_2);
+
+    fflush(gp); // Clean up Data
+
+    fprintf(gp, "exit\n"); // Quit gnuplot
+
+    pclose(gp);
+}
+
+void gnuplot_3()
+{
+    /** Gnuplot **/
+
+    // ファイル名
+    char readfile[] = "data/error_2-50.dat";
+    char graphfile[] = "results/error_3.svg";
+    char graphtitle[] = "Number of blue particles in one image : {/:Italic n} = 50 [-/frame]";
+
+    printf("READ FILE (1) : %s\n", readfile);
+    printf("GRAPH FILE    : %s\n", graphfile);
+
+    // 軸の設定
+
+    // range x
+    const float x_min = 5.5;
+    const float x_max = 14.5;
+
+    // range y
+    const float y_min = 1.0;
+    const float y_max = 4.0;
+
+    // label
+    const char *xxlabel = "Rotational speed : {/Symbol w} × 180 / {/Symbol p} [deg/s]";
+    const char *yylabel = "RMSE rate [%]";
+
+    // Gnuplot 呼び出し
+    if ((gp = popen("gnuplot", "w")) == NULL)
+    {
+        printf("gnuplot is not here!\n");
+        exit(0); // gnuplotが無い場合、異常ある場合は終了
+    }
+
+    fprintf(gp, "set terminal svg enhanced size 500, 500 font 'Times New Roman, 18'\n");
+    fprintf(gp, "set size ratio 0.8\n");
+
+    // 出力ファイル
+    fprintf(gp, "set output '%s'\n", graphfile);
+
+    // 非表示
+    fprintf(gp, "unset key\n");
+
+    // 軸の表記桁数の指定
+    fprintf(gp, "set format x '%%.0f'\n");
+    fprintf(gp, "set format y '%%.1f'\n");
+
+    // 軸の範囲
+    fprintf(gp, "set xrange [%.3f:%.3f]\n", x_min, x_max);
+    fprintf(gp, "set yrange [%.3f:%.3f]\n", y_min, y_max);
+
+    // 軸ラベル
+    fprintf(gp, "set xlabel '%s' offset 0.0, 0.5\n", xxlabel);
+    fprintf(gp, "set ylabel '%s' offset 0.5, 0.0\n", yylabel);
+
+    // 軸の数値位置
+    fprintf(gp, "set xtics 1 offset 0.0, 0.2\n");
+    fprintf(gp, "set ytics 0.5 offset 0.0, 0.0\n");
+
+    // グラフの出力
+    fprintf(gp, "plot '%s' using 1:4 with points pt 7 ps 0.5 lc 'red' notitle, '%s' using 1:4 with lines lw 0.5 lc 'red' notitle\n ", readfile, readfile);
+
+    fflush(gp); // Clean up Data
+
+    fprintf(gp, "exit\n"); // Quit gnuplot
+
+    pclose(gp);
+}
+
+void gnuplot_4()
+{
+    /** Gnuplot **/
+
+    // ファイル名
+    char readfile[] = "data/error_2-300.dat";
+    char graphfile[] = "results/error_4.svg";
+    char graphtitle[] = "Number of blue particles in one image : {/:Italic n} = 50 [-/frame]";
+
+    printf("READ FILE (1) : %s\n", readfile);
+    printf("GRAPH FILE    : %s\n", graphfile);
+
+    // 軸の設定
+
+    // range x
+    const float x_min = 5.5;
+    const float x_max = 14.5;
+
+    // range y
+    const float y_min = 4.0;
+    const float y_max = 7.0;
+
+    // label
+    const char *xxlabel = "Rotational speed : {/Symbol w} × 180 / {/Symbol p} [deg/s]";
+    const char *yylabel = "RMSE rate [%]";
+
+    // Gnuplot 呼び出し
+    if ((gp = popen("gnuplot", "w")) == NULL)
+    {
+        printf("gnuplot is not here!\n");
+        exit(0); // gnuplotが無い場合、異常ある場合は終了
+    }
+
+    fprintf(gp, "set terminal svg enhanced size 500, 500 font 'Times New Roman, 18'\n");
+    fprintf(gp, "set size ratio 0.8\n");
+
+    // 出力ファイル
+    fprintf(gp, "set output '%s'\n", graphfile);
+
+    // 非表示
+    fprintf(gp, "unset key\n");
+
+    // 軸の表記桁数の指定
+    fprintf(gp, "set format x '%%.0f'\n");
+    fprintf(gp, "set format y '%%.1f'\n");
+
+    // 軸の範囲
+    fprintf(gp, "set xrange [%.3f:%.3f]\n", x_min, x_max);
+    fprintf(gp, "set yrange [%.3f:%.3f]\n", y_min, y_max);
+
+    // 軸ラベル
+    fprintf(gp, "set xlabel '%s' offset 0.0, 0.5\n", xxlabel);
+    fprintf(gp, "set ylabel '%s' offset 0.5, 0.0\n", yylabel);
+
+    // 軸の数値位置
+    fprintf(gp, "set xtics 1 offset 0.0, 0.2\n");
+    fprintf(gp, "set ytics 0.5 offset 0.0, 0.0\n");
+
+    // グラフの出力
+    fprintf(gp, "plot '%s' using 1:4 with points pt 7 ps 0.5 lc 'blue' notitle, '%s' using 1:4 with lines lw 0.5 lc 'blue' notitle\n ", readfile, readfile);
 
     fflush(gp); // Clean up Data
 
